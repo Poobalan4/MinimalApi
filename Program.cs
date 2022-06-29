0445c118ae3420a59a5eb13d013f7f5a1f8992c0
Poobalan4 <poobalan410@gmail.com>
2022-06-29 17:32:36 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policy =>
+        {
+            policy.WithOrigins("*");
+        });
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
